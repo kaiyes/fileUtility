edefb8689189d483cd30230cfadafc5f7ed61815
kaiyes <kaiyes.ansary@gmail.com>
2020-08-09 19:18:58 +0600

    at least it works

--- index.js
+++ index.js
@@ -10,31 +10,23 @@ const port = 3000
 async function addDeleteTest() {
 	let futureArr = []
 	const pages = await Array.from(Array(604).keys())
-	let cutOutArr = pages.slice(0, 30)
-	cutOutArr.forEach(async (page) => {
-		//fetching
+	let cutOutArr = pages.slice(0, 10)
 
+	function finished() {
+		console.log('finished')
+	}
+	let ultimateArr = []
+	for (let page of cutOutArr) {
 		let data = await fetch(`http://api.alquran.cloud/v1/page/${page + 1}`, {
 			method: 'Get'
 		})
-
 		let jsonifiedData = await data.json()
+		await ultimateArr.push(jsonifiedData.data.ayahs)
+	}
+	console.log(ultimateArr)
 
-		futureArr.push(jsonifiedData.data)
-
-		console.log(futureArr)
-
-		//upon finished
-		function finished() {
-			console.log(`finished page: ${page}`)
-		}
-
-		await writeFile(
-			`page${page + 1}.json`,
-			JSON.stringify(futureArr, null),
-			finished
-		)
-	})
+	//	writing the array to a new file
+	await writeFile(`page-0-10.json`, JSON.stringify(ultimateArr, null), finished)
 }
 
 app.listen(port, () => {
