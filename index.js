a0a38a820078bf972119abf4bed562990b39d297
kaiyes <kaiyes.ansary@gmail.com>
2020-08-10 19:09:32 +0600

    uptp 40 page

--- index.js
+++ index.js
@@ -10,23 +10,29 @@ const port = 3000
 async function addDeleteTest() {
 	let futureArr = []
 	const pages = await Array.from(Array(604).keys())
-	let cutOutArr = pages.slice(0, 10)
+	let cutOutArr = pages.slice(30, 40)
+	let pageNumber = '30-40'
 
 	function finished() {
 		console.log('finished')
 	}
+
 	let ultimateArr = []
 	for (let page of cutOutArr) {
 		let data = await fetch(`http://api.alquran.cloud/v1/page/${page + 1}`, {
 			method: 'Get'
 		})
 		let jsonifiedData = await data.json()
-		await ultimateArr.push(jsonifiedData.data.ayahs)
+		await ultimateArr.push(jsonifiedData.data)
 	}
 	console.log(ultimateArr)
 
 	//	writing the array to a new file
-	await writeFile(`page-0-10.json`, JSON.stringify(ultimateArr, null), finished)
+	await writeFile(
+		`page-${pageNumber}.json`,
+		JSON.stringify(ultimateArr, null),
+		finished
+	)
 }
 
 app.listen(port, () => {
