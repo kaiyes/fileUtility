d90719c7d7f42449a7a23aef769635134d7f1abf
kaiyes <kaiyes.ansary@gmail.com>
2020-08-26 16:59:58 +0600

    fetched 200 more

--- index.js
+++ index.js
@@ -8,8 +8,8 @@ const app = express()
 const port = 3000
 
 async function mergeAll() {
-	const bn = await loadJsonFile('dua_bn_2.json')
-	const en = await loadJsonFile('dua_en.json')
+	const bn = await loadJsonFile('dua_bn_new.json')
+	const en = await loadJsonFile('dua_en_new.json')
 
 	const mergedData = await merge(bn, en)
 
@@ -18,6 +18,36 @@ async function mergeAll() {
 	}
 	// making separate files by mapping
 	await writeFile('merged.json', JSON.stringify(mergedData, null), finished)
+}
+
+async function fetchData() {
+	const allDuas = await Array.from(Array(328).keys())
+
+	let cutOutArr = allDuas.slice(200)
+
+	let ultimateArr = []
+	for (let dua of cutOutArr) {
+		let data = await fetch(
+			`https://dua.greentechapps.com/api/bn/details/${dua + 1}`,
+			{
+				method: 'Get'
+			}
+		)
+
+		let jsonifiedData = await data.json()
+
+		await ultimateArr.push(jsonifiedData)
+	}
+
+	function finished() {
+		console.log('finished')
+	}
+
+	await writeFile(
+		`./dua_bn_new.json`,
+		JSON.stringify(ultimateArr, null),
+		finished
+	)
 }
 
 app.listen(port, () => {
